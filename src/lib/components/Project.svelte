5844b36aa9161afe2dfe9a5eb2d329ff8b198276
tom-2810 <tom.poortman@hva.nl>
2023-09-08 21:40:50 +0200

    🎊 UPDATE ProjectList

---
+++ src/lib/components/Project.svelte
@@ -0,0 +1,107 @@
+<script>
+    export let item;
+</script>
+
+<section class="project">
+	<a href="/project{item.project.url}">
+		<h3>{item.project.data.title}</h3>
+		<img src={item.project.data.hero_image.url} alt="" />
+
+		<p class="case-study">
+			Read case study
+			<svg>
+				<defs>
+					<marker id="m" markerWidth="4" markerHeight="5" refX="0" refY="1" viewBox="0 0 1 2">
+						<polygon points="0,0 1,1 0,2" fill="#ff9900" />
+					</marker>
+				</defs>
+				<line
+					x1="0"
+					y1="50%"
+					x2="100%"
+					y2="50%"
+					stroke-width="1.5"
+					marker-end="url(#m)"
+					stroke="#ff9900"
+				/>
+			</svg>
+		</p>
+	</a>
+</section>
+
+<style>
+	svg {
+		width: 20px;
+		height: 20px;
+		overflow: visible;
+	}
+
+	.project {
+		position: relative;
+		transition: .7s;
+	}
+
+	.project:nth-child(1n) {
+		grid-column: span 5;
+	}
+	
+	.project:nth-child(2n) {
+		grid-column: span 4;
+		transform: translateY(13rem);
+	}
+
+	.project:nth-child(3n) {
+		grid-column: span 7;
+		transform: translateX(6rem) translateY(1rem);
+	}
+
+	.project:nth-child(4n) {
+		grid-column: span 4;
+		transform: translateY(8rem);
+	}
+
+	.project:nth-child(5n) {
+		grid-column: span 5;
+		transform: translateY(-4rem);
+	}
+
+	.project:hover svg {
+		width: 3rem;
+	}
+
+	.project * {
+		transition: 0.5s;
+	}
+
+	.project .case-study {
+		display: flex;
+		gap: var(--size-s);
+		font-size: 1.1rem;
+		font-weight: lighter;
+		color: #ff9900;
+	}
+
+	.project:hover {
+		scale: 1.08;
+	}
+
+	.project:hover :is(img, .summary) {
+		border-radius: var(--radius-s);
+		color: var(--c-global-text);
+	}
+
+	.project img {
+		width: 100%;
+		aspect-ratio: 2/1.5;
+		object-fit: cover;
+		/* margin: .2rem 0; */
+
+		border-radius: var(--radius-xl);
+	}
+
+	@media (max-width: 580px) {
+		.project {
+			transform: translateX(0) translateY(0) !important;
+		}
+	}
+</style>
--- src/lib/slices/ProjectList/index.svelte
+++ src/lib/slices/ProjectList/index.svelte
@@ -2,6 +2,10 @@
   /** @type {import("@prismicio/client").Content.ProjectListSlice} */
   export let slice;
 
+  import Project from "$lib/components/Project.svelte";
+
+	let currentAmountOfProjects = 3;
+
   console.log(slice.items[0]);
 </script>
 
@@ -9,16 +13,57 @@
   data-slice-type={slice.slice_type}
   data-slice-variation={slice.variation}
 >
-  {#each slice.items as item}
-  <a href=/project{item.project.url}>
-    <h3>{item.project.data.title}</h3>
-    <img src={item.project.data.hero_image.url} alt="" />
-  </a>
-  {/each}
+  <h2><a href="#projects">Projects</a></h2>
+
+  <div class="projects">
+    {#each slice.items.slice(0, currentAmountOfProjects) as item}
+      <Project {item} />
+    {/each}
+  </div>
+
+  <div class="more">
+    {#if currentAmountOfProjects < slice.items.length}
+      <button
+        class="second-btn"
+        on:click={() => (currentAmountOfProjects = currentAmountOfProjects + 3)}
+      >
+        Show
+        {#if slice.items.length - currentAmountOfProjects > 3}
+          {3}
+        {:else}
+          {slice.items.length - currentAmountOfProjects}
+        {/if}
+        more
+      </button>
+    {/if}
+  </div>
 </section>
 
 <style>
-  img {
-    width: 23rem;
+  .projects {
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+    column-gap: var(--size-xl);
+    row-gap: var(--size-xxl);
+
+    margin: 0 auto 4rem auto;
+    width: 100%;
+    max-width: 53rem;
+    padding: 0 5vw;
+  }
+
+  .more {
+    display: flex;
+    justify-content: space-around;
+
+    font-family: "Fugaz One", cursive;
+  }
+
+  @media (max-width: 580px) {
+    .projects {
+      display: flex;
+      flex-direction: column;
+      gap: var(--size-xl);
+    }
   }
 </style>
