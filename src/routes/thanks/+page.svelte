3abdccf6898a596e4c14a3e1c4dd0a7e43f23fd2
tom-2810 <tpoortman10@gmail.com>
2024-07-30 00:11:34 +0200

    thanks page

--- src/routes/thanks/+page.svelte
+++ src/routes/thanks/+page.svelte
@@ -1,1 +1,39 @@
-<h1>Thanks</h1>
+<section>
+    <div class="card">
+        <h1>Yes! 🎉 Je bericht is verzonden</h1>
+        <p>Ik probeer altijd binnen twee werkdagen te reageren.</p>
+    </div>
+</section>
+
+<style>
+    section {
+        position: absolute;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+        height: 100vh;
+        width: 100%;
+        background-color: rgb(255, 171, 35);
+        padding: 1rem;
+    }
+    .card {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        background-color: white;
+        padding: 3rem;
+        max-width: 26rem;
+        border-radius: 12px;
+    }
+    h1 {
+        font-size: 2rem;
+        color: rgb(40, 40, 40);
+    }
+    p {
+        font-size: 1.2rem;
+        color: rgba(40, 40, 40);
+    }
+</style>
