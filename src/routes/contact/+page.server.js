293ab258d6ecf10e8f5465e6d14b88d1a1310440
tom-2810 <tpoortman10@gmail.com>
2024-07-30 20:22:58 +0200

    Update +page.server.js

--- src/routes/contact/+page.server.js
+++ src/routes/contact/+page.server.js
@@ -19,21 +19,21 @@ let origin
 //     throw redirect(307, `${url.origin}/contact`);
 // }
 
-export async function load({ url, params }) {
-    origin = url.origin
-    console.log(url.origin)
+// export async function load({ url, params }) {
+//     origin = url.origin
+//     console.log(url.origin)
 
-    fetch(origin + "/contact", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: {
-            "form-name": "contact",
-            "name": "naampie",
-        },
-    })
-        .then(() => {
-            console.log("submitted");
-        })
-        .catch((error) => console.log(error));
-}
+//     fetch(origin + "/contact", {
+//         method: "POST",
+//         headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//         body: {
+//             "form-name": "contact",
+//             "name": "naampie",
+//         },
+//     })
+//         .then(() => {
+//             console.log("submitted");
+//         })
+//         .catch((error) => console.log(error));
+// }
 
