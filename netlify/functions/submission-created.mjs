176b637d84cc1e2c61c037e062edaf2b414ea1b2
tom-2810 <tpoortman10@gmail.com>
2024-07-30 13:27:40 +0200

    Update submission-created.mjs

--- netlify/functions/submission-created.mjs
+++ netlify/functions/submission-created.mjs
@@ -1,3 +1,16 @@
-export default async (req, context) => {
-    return new Response.error();
-};
+export async function handler(event, context) {
+    // This function will be triggered when a form submission is verified
+  
+    // The form data is included in the event body
+    const formData = JSON.parse(event.body).payload;
+  
+    
+    // You can add your own custom logic here...
+  
+  
+    // The function should return a response
+    return {
+      statusCode: 200,
+      body: "Form submission received",
+    };
+  }
