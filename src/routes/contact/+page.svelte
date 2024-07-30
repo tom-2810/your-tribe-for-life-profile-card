17cbb926d2cb823ca99561aaf032068aa5a04493
tom-2810 <tpoortman10@gmail.com>
2024-07-30 17:49:18 +0200

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,16 +1,36 @@
+<script>
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        let body = new URLSearchParams(formData);
+
+        console.log("submitting...");
+
+        fetch("/contact", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: body,
+        })
+            .then(() => (alert("verzonden")))
+            .catch((error) => alert(error));
+    };
+</script>
+
 <form
     method="POST"
     name="contact-footer"
     netlify
     netlify-honeypot="bot-field"
-    action="/thanks"
+    on:submit={handleSubmit}
 >
     <input type="hidden" name="form-name" value="contact-footer" />
     <div class="form__inputs">
-        <input type="text" name="first-name" />
-        <input type="text" name="last-name" />
-        <input type="email" name="email" />
-        <input type="tel" name="phone" />
-        <textarea name="message"></textarea>
+        <input type="text" name="first-name" value="voornaam" />
+        <input type="text" name="last-name" value="achternaam" />
+        <input type="email" name="email" value="voornaam@gmail.com" />
     </div>
+    <button>stuur</button>
 </form>
